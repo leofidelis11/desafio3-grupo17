45d0a0ed8b6d0377cdecc113995b4553fb176494
Adriana <bussoloadriana@gmail.com>
2025-10-06 16:45:30 -0300

    Adicionado primeiro teste de performance de get/user

--- performance/register.test.js
+++ performance/register.test.js
@@ -2,32 +2,35 @@ import http from 'k6/http'
 import { sleep, check } from 'k6'
 
 export const options = {
-    VUS: 200,
-    duration: '20s',
-    thresholds: {
-        http_req_duration: ['p(95)<2000'],
-    },
+  VUS: 200,
+  duration: '20s',
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+  },
 }
 
 export default function () {
-    const url = 'http://localhost:3000/users/register';
+  const url = 'http://localhost:3000/users/register';
 
-    const payload = JSON.stringify({
-        username: 'julio',
-        password: '123456'
-      })
-  
-    const params = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  
-    const resposta = http.post(url, payload, params);
+  const payload = JSON.stringify({
+    username: 'julio',
+    password: '123456'
+  })
 
-    check(resposta, {
-        'Validar que o status code é 400': (r) => r.status === 400,
-    })
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
 
-    sleep(1)
+  const resposta = http.post(url, payload, params);
+
+  check(resposta, {
+    'Validar que o status code é 400': (r) => r.status === 400,
+  })
+
+  sleep(1)
 }
+
+
+
